3341195b625c3c35f55282d0408237a741fd746a
nicolekoh <niicolekws@gmail.com>
2016-05-22 19:31:37 +0800

    added figaro gem &clearance reset password working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+ config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV["gmail_username"],
+ :password             => ENV["gmail_password"],
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -14,7 +25,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,5 @@
 Clearance.configure do |config|
-  config.mailer_sender = "reply@example.com"
+  config.mailer_sender = "myfoodroulette@gmail.com"
+
 end
+
