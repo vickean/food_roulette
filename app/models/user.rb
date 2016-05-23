0dca7a2f416ace8f55f2ebc1210b5ff2ea889f41
nicolekoh <niicolekws@gmail.com>
2016-05-23 17:04:38 +0800

    Added facebook login with omniauth-facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,25 @@
-class User <   include Clearance::User
+class User < ActiveRecord::Base
+  has_many :authentications, :dependent => :destroy
 
-ActiveRecord::Base
   include Clearance::User
+
+
+  def self.create_with_auth_and_hash(authentication,auth_hash)
+    create! do |u|
+      #u.first_name = auth_hash["info"]["first_name"]
+      u.email = auth_hash["extra"]["raw_info"]["email"]
+      u.encrypted_password = [*('A'..'Z')].sample(8).join
+      u.authentications<<(authentication)
+    end
+  end
+
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+
+  def password_optional?
+    true
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
 
-  get 'users/edit'
 
-  get 'welcome/index'
+  resources :users, only: [:show, :edit, :update, :destroy] 
+
+ 
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
