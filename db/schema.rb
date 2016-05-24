4106a15fa17a5231e0792fe4c16c17bd95c56583
Behenghui1987 <behenghui@gmail.com>
2016-05-24 15:11:11 +0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523103055) do
+ActiveRecord::Schema.define(version: 20160524043050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,20 +41,19 @@ ActiveRecord::Schema.define(version: 20160523103055) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.string   "email",                                      null: false
+    t.string   "encrypted_password", limit: 128,             null: false
     t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
+    t.string   "remember_token",     limit: 128,             null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "gender"
     t.string   "avatar"
     t.text     "description"
-    t.integer  "low_spin_num"
-    t.integer  "med_spin_num"
-    t.integer  "hi_spin_num"
+    t.integer  "user_type",                      default: 0
+    t.integer  "spin_num"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
