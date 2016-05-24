9bff5f3e70193a1440357497e3138a664e5ba739
Behenghui1987 <behenghui@gmail.com>
2016-05-24 19:48:37 +0800

    admin done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,11 @@
 class Restaurant < ActiveRecord::Base
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :no_of_guest, presence: true
+	validates :price_per_person, presence: true
+	validates :address, presence: true
+	validates :latitude, presence: true
+	validates :longitude, presence: true
+	validates :contact_number, presence: true
+	validates :price_tier, presence: true
 end
