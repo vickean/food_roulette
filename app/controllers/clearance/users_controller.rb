c8cf2362799cddfee043aac4587daaa202f05484
Behenghui1987 <behenghui@gmail.com>
2016-05-25 14:30:03 +0800

    new algorithm

--- app/controllers/clearance/users_controller.rb
+++ app/controllers/clearance/users_controller.rb
@@ -12,7 +12,7 @@ class Clearance::UsersController < ApplicationController
 
   def new
     @user = user_from_params
-    render template: "users/new"
+    render template: "clearance/users/new"
   end
 
   def create
@@ -22,7 +22,7 @@ class Clearance::UsersController < ApplicationController
       sign_in @user
       redirect_back_or url_after_create
     else
-      render template: "users/new"
+      render template: "clearance/users/new"
     end
   end
 
